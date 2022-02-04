398750f52b8fcad062bbbed19b3ec4f9f4f255e6
DeenaTTA <93340885+DeenaTTA@users.noreply.github.com>
2022-02-04 01:05:27 +0000

    Create R-HLT2.R

---
+++ R-HLT2.R
@@ -0,0 +1,6 @@
+Name<-c('Harry potter','lisa smith','Morag Green','Alice Green','Peterloyd')
+Age<-c(48,37,28,55,46)
+Role<-c(DataAnalyst,SrTechinician,JrDeveloper,Team Leader,DataScientist)
+LengthofService<-c(15,10,5,20,12)
+df<-data.frame(Name,Age,Role,LengthofService)
+print(df)
